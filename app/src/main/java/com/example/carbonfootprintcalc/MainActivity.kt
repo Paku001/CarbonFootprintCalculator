192b87e8641154433d18ef9b0a5126793c423e71
Paku <gangaraju5316@gmail.com>
2022-10-08 12:09:21 +0530

    commit 2

--- app/src/main/java/com/example/carbonfootprintcalc/MainActivity.kt
+++ app/src/main/java/com/example/carbonfootprintcalc/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.carbonfootprintcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.carbonfootprintcalc.ui.theme.CarbonFootprintCalcTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,14 +41,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    var electricityInput by remember { mutableStateOf("") }
+    val electricity = electricityInput.toDoubleOrNull()?: 0.0
+
+    Column(
+        modifier = Modifier.padding(34.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Carbon Footprint Calculator",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        EntryInput(
+            value = electricityInput,
+            onvaluechange = {electricityInput = it},
+            label = "Electricity consumption in Kw/H"
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun EntryInput(
+    value: String,
+    onvaluechange: (String) -> Unit,
+    label: String,
+) {
+    TextField(
+        value = value,
+        onValueChange = onvaluechange,
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        label = { Text(text = label)},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+
+    )
+
+}
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     CarbonFootprintCalcTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
